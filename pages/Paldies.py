e8077a69db523608ea38002288af8f856c7165d7
JhonMcLovin <124922746+JhonMcLovin@users.noreply.github.com>
2023-09-14 02:31:00 +0300

    Update Paldies.py

--- pages/Paldies.py
+++ pages/Paldies.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("Liels paldies par šo kursu")
-st.write("Bija interesanti ļoti interesanti.")
+st.write("Bija ļoti interesanti.")
 st.write("Un, lai gan neatcerēšos visus operatorus, formulas, formātus un citas lietas no galvas, pamati ir ielikti un esmu pārliecināts ka mācēšu pareizi izteikties meklēšanas pārlūkos, lai spētu atrast sev vajadzīgos risinājumus un koda blokus.")
