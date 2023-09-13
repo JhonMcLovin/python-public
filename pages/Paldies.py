a0c9fffe1bb8567dc76d429a6157b47001538c9f
JhonMcLovin <124922746+JhonMcLovin@users.noreply.github.com>
2023-09-14 02:23:32 +0300

    Add files via upload

---
+++ pages/Paldies.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Liels paldies par šo kursu")
+st.write("Bija interesanti ļoti interesanti.")
+st.write("Un, lai gan neatcerēšos visus operatorus, formulas, formātus un citas lietas no galvas, pamati ir ielikti un esmu pārliecināts ka mācēšu pareizi izteikties meklēšanas pārlūkos, lai spētu atrast sev vajadzīgos risinājumus un koda blokus.")
