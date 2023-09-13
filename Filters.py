097f41a7cd410bfba7ef06b9de4eb3971b08f950
JhonMcLovin <124922746+JhonMcLovin@users.noreply.github.com>
2023-09-13 14:57:46 +0300

    Add files via upload

--- Filters.py
+++ Filters.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-@st.cache_data # Caching to improve performance
 def load_data():
     data = pd.read_csv(r"C:\Users\janis\Desktop\Nosleguma_Darbs\Streamlit\data\board_games.csv")
     return data
@@ -11,16 +10,22 @@ data = load_data()
 
 unique_group_names = data['Board game type'].unique()
 
+# Create a Streamlit app
+st.title("Board Games")
+
 add_selectbox = st.sidebar.selectbox(
     "What type of board games would you like to see?",
-    (unique_group_names)
+    unique_group_names
 )
 
 # Filter the data based on the selected value
 filtered_data = data[data['Board game type'] == add_selectbox]
 
-# Create a Streamlit app
-st.title(f"Board Games of Type: {add_selectbox}")
+# Count the number of games and unique manufacturers
+games_count = filtered_data['Board game name'].nunique()
+man_count = filtered_data['Board game manufacturer'].nunique()
+
+# Display the list of board games
 st.write(f"List of board games of type '{add_selectbox}':")
 st.write(filtered_data)
 
@@ -34,5 +39,7 @@ concatenated_text2 = ', '.join(concatenated_man)
 st.write(f"Concatenated 'Board game name' values for '{add_selectbox}':")
 st.text(concatenated_text)
 
-st.write(f"Concatenated 'Board game name' values for '{add_selectbox}':")
+st.write(f"Concatenated 'Board game manufacturer' values for '{add_selectbox}':")
 st.text(concatenated_text2)
+
+st.write(f"When it comes to {add_selectbox} games, you can choose from {games_count} games made by {man_count} different manufacturers")
