ce350f27f8a87a00b044cbf0e116f9d0db396d34
JhonMcLovin <124922746+JhonMcLovin@users.noreply.github.com>
2023-09-13 23:08:56 +0300

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -9,29 +9,29 @@ df = pd.read_csv(url)
 st.title("COVID-19 Data Filtering App")
 st.markdown("This app allows you to filter COVID-19 data from 2023.")
 
-# Sidebar filters
-st.sidebar.header("Filter Options")
+# # Sidebar filters
+# st.sidebar.header("Filter Options")
 
-# Filter by country
-country = st.sidebar.selectbox("Select Country", df["location"].unique())
+# # Filter by country
+# country = st.sidebar.selectbox("Select Country", df["location"].unique())
 
-# Filter by date with "DD.MM.YYYY" format
-start_date = st.sidebar.date_input("Start Date", format="DD.MM.YYYY")
-end_date = st.sidebar.date_input("End Date", format="DD.MM.YYYY")
+# # Filter by date with "DD.MM.YYYY" format
+# start_date = st.sidebar.date_input("Start Date", format="DD.MM.YYYY")
+# end_date = st.sidebar.date_input("End Date", format="DD.MM.YYYY")
 
-# Apply filters
-filtered_df = df[(df["location"] == country) & (df["date"] > start_date) & (df["date"] < end_date)]
+# # Apply filters
+# filtered_df = df[(df["location"] == country) & (df["date"] > start_date) & (df["date"] < end_date)]
 
 # Display the filtered data
-st.header("Filtered Data")
-st.write(filtered_df)
+# st.header("Filtered Data")
+st.write(df)
 
 # Download filtered data as CSV
-if st.button("Download Filtered Data as CSV"):
-    with st.spinner("Downloading..."):
-        filtered_df.to_csv(f"{country}_covid_data.csv", index=False)
-        st.success(f"{country}_covid_data.csv downloaded successfully!")
+# if st.button("Download Filtered Data as CSV"):
+#     with st.spinner("Downloading..."):
+#         filtered_df.to_csv(f"{country}_covid_data.csv", index=False)
+#         st.success(f"{country}_covid_data.csv downloaded successfully!")
 
 # Show the original data
-st.header("Original Data")
-st.write(df)
+# st.header("Original Data")
+# st.write(df)
